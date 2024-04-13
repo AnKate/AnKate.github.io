0effecd9d6c0f3437fa838bbe5a666e3f3b5c284
AnKate <miheal_keehl@foxmail.com>
2024-04-13 13:50:54 +0800

    04/13: new blog init

--- src/config.ts
+++ src/config.ts
@@ -32,8 +32,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'AnKate',
+  bio: 'Laugh hard. Run fast. Be kind.',
   links: [
     // {
     //   name: 'Twitter',
